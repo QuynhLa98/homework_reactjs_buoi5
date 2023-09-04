e8c75dbff57f4835006017888b2734c887a4c1b2
QuynhLa98 <quynhla98@gmail.com>
2023-09-04 19:44:48 +0700

    update code

---
+++ baitap_reactform/src/store/reducers/sinhVienReducer.js
@@ -0,0 +1,78 @@
+import danhSachSinhVien from "../../data/danhSachSinhVien.json";
+import { ADD_USER, DELETE_USER, SET_SELECTED_USER, UPDATE_USER } from "../types/userType";
+const DEFAULT_STATE = {
+  sinhVienList: danhSachSinhVien,
+  selectedUser: null,
+};
+const stringify = localStorage.getItem("SINHVIEN_LIST");
+if (stringify) {
+  DEFAULT_STATE.sinhVienList = JSON.parse(stringify);
+}
+export const sinhVienReducer = (state = DEFAULT_STATE, action) => {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case ADD_USER: {
+      if (
+        window.confirm(
+          `Bạn có chắc muốn thêm sinh viên ${action.payload.maSV} không?`
+        )
+      ) {
+        state.sinhVienList = [...state.sinhVienList, action.payload];
+        localStorage.setItem(
+          "SINHVIEN_LIST",
+          JSON.stringify(state.sinhVienList)
+        );
+        break;
+      }
+      break;
+    }
+    // eslint-disable-next-line no-fallthrough
+    case SET_SELECTED_USER: {
+      state.selectedUser = action.payload;
+      break;
+    }
+    case UPDATE_USER: {
+      if (
+        window.confirm(
+          `Bạn có chắc muốn cập nhật cho sinh viên ${action.payload.maSV} không?`
+        )
+      ) {
+        const data = [...state.sinhVienList];
+        const index = data.findIndex(
+          (element) => element.maSV === action.payload.maSV
+        );
+        data[index] = action.payload;
+        state.selectedUser = null;
+        state.sinhVienList = data;
+        localStorage.setItem(
+          "SINHVIEN_LIST",
+          JSON.stringify(state.sinhVienList)
+        );
+        break;
+      }
+      break;
+    }
+    // eslint-disable-next-line no-fallthrough
+    case DELETE_USER: {
+      if (
+        window.confirm(
+          `Bạn có chắc muốn xóa sinh viên ${action.payload.maSV} không?`
+        )
+      ) {
+        const data = [...state.sinhVienList];
+        const index = data.findIndex(
+          (element) => element.maSV === action.payload.maSV
+        );
+        data.splice(index, 1);
+        state.sinhVienList = data;
+        localStorage.setItem(
+          "SINHVIEN_LIST",
+          JSON.stringify(state.sinhVienList)
+        );
+        break;
+      }
+      break;
+    }
+  }
+  return { ...state };
+};
